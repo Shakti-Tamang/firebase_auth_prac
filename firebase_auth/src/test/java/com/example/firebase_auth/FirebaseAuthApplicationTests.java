ba2f8ea0d89b4e473c7452ea64cccb6c4aaa66ff
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-11-23 15:23:06 +0545

    feat:firebase auth setup

---
+++ firebase_auth/src/test/java/com/example/firebase_auth/FirebaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebase_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/example/firebase_auth/FirebaseAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseAuthApplication.class, args);
+	}
+
+}
