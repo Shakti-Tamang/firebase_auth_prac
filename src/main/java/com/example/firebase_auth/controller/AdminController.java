0504887821b4deca251679cd9b596094bc133beb
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-11-24 23:59:18 +0545

    feat: get by id

--- src/main/java/com/example/firebase_auth/controller/AdminController.java
+++ src/main/java/com/example/firebase_auth/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.example.firebase_auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +17,7 @@ public class AdminController {
     }
 
           @GetMapping("/getAdmin")
-    public String testGet(){
+    public String testGet(@RequestParam("id") int id){
 
         
         return "success hlo";
